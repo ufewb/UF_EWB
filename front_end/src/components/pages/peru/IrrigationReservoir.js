378887a0aba1e12edff2c041bc080b83f480e8f5
elinakocarslan <148897716+elinakocarslan@users.noreply.github.com>
2024-05-23 12:21:44 -0400

    changes in peru pages

--- front_end/src/components/pages/peru/IrrigationReservoir.js
+++ front_end/src/components/pages/peru/IrrigationReservoir.js
@@ -3,7 +3,14 @@ import React from 'react';
 function IrrigationReservoir() {
     return (
         <div>
-            <h1>Irrigation Reservoir</h1>
+            <div className='mainText'>
+                <h1>Irrigation Reservoir</h1>
+                <h2>2018-2022</h2>
+            </div>
+            <div className='buttons'>
+                <button className='buttonPeruHome'Peru Home Page></button>
+                <button className='buttonVDG'Virgen Del Carmen Elementary></button>
+            </div>
         </div>
     );
 }
