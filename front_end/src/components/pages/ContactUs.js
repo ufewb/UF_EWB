7c074b24c047ce8aca343c9abb37edfcfc8390d6
jonathandeng7 <jmdeng05@gmail.com>
2024-05-23 13:49:50 -0400

    contactus

--- front_end/src/components/pages/ContactUs.js
+++ front_end/src/components/pages/ContactUs.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import "./ContactUs.css";
 
-function ContactUs () {
+import LogoPictureCU from "./pngwing.com.png";
+
+function ContactUs() {
     return (
-        <div>
-            <h1> Contact Us </h1>
+        <div className="entireBox">
+
+            <div className="NameBox">
+                <h1>Contact Us</h1>
+            </div>
+
+            <div className='LowerHalf'>
+                <div className="PictureBox">
+                    <img 
+                        src = {LogoPictureCU}
+                        className="logo1" 
+                        alt="Logo"
+                    />
+                </div>
+                <div className='ContactInfo'>
+                    <div className='Info'>
+                        <p>
+                            <label>Name:</label><br />
+                            <input type="text" placeholder="Enter your name" />
+                        </p>
+                        <p>
+                            <label>Email:</label><br />
+                            <input type="email" placeholder="Enter your email" />
+                        </p>
+                        <p>
+                            <label>Message:</label><br />
+                            <textarea placeholder="Enter your message" rows="4"></textarea>
+                        </p>
+                        <button>Contact</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
