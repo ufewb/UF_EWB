1d0fd57bae3306f183b2d63e8c687101c7365910
elinakocarslan <148897716+elinakocarslan@users.noreply.github.com>
2024-08-14 13:16:41 -0400

    pushing updates

--- front_end/src/components/pages/GetInvolved.js
+++ front_end/src/components/pages/GetInvolved.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './GetInvolved.css';
 
-function GetInvolved () {
+import randomImg from './peru/pictures/randomPic.png';
+
+function GetInvolved() {
+    const [openIndex, setOpenIndex] = useState(null);
+
+    const toggleFAQ = (index) => {
+        setOpenIndex(openIndex === index ? null : index);
+    };
+
     return (
         <div>
-            <h1> Get Involved </h1>
+            <div className='intro-box'>
+                <div className='intro-writing'>
+                    <h1>Get Involved</h1>
+                </div>
+            </div>
+
+            <div className='info-box'>
+                <div className='application-and-pic'>
+                    <div className='caption'>
+                        <h1>APPLICATION</h1>
+                        <div className='paragraph'>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel nunc quis orci elementum rhoncus. Curabitur sodales velit felis, in vulputate mi porttitor ac. Maecenas quis nisl quis odio efficitur imperdiet. Proin ac gravida mi. Fusce interdum quam vitae odio tempor facilisis. Aliquam erat volutpat. Phasellus tristique turpis ut euismod consectetur. Donec et neque molestie eros dictum mattis sed quis risus. Curabitur faucibus malesuada urna, et gravida lectus fringilla non. Nunc pellentesque placerat faucibus. Fusce gravida odio non sapien aliquet vehicula.</p>
+                        </div>
+                    </div>
+                    <img src={randomImg} alt="Home Image" className='applicationPicImg' />
+                </div>
+            </div>
+
+            <div className='FAQ-box'>
+                <div className='caption'>
+                    <h1>FAQ</h1>
+                </div>
+
+                {[...Array(5)].map((_, index) => (
+                    <div className='faq' key={index}>
+                        <button className='Q-Box' onClick={() => toggleFAQ(index)}>
+                            Q. Lorem ipsum dolor sit amet, consectetur adipiscing elit?
+                        </button>
+                        <div className={`answer ${openIndex === index ? 'active' : ''}`}>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
--- front_end/src/components/pages/peru/IrrigationReservoir.js
+++ front_end/src/components/pages/peru/IrrigationReservoir.js
@@ -3,14 +3,7 @@ import React from 'react';
 function IrrigationReservoir() {
     return (
         <div>
-            <div className='mainText'>
-                <h1>Irrigation Reservoir</h1>
-                <h2>2018-2022</h2>
-            </div>
-            <div className='buttons'>
-                <button className='buttonPeruHome'Peru Home Page></button>
-                <button className='buttonVDG'Virgen Del Carmen Elementary></button>
-            </div>
+            <h1>Irrigation Reservoir</h1>
         </div>
     );
 }
