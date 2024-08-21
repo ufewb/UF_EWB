c564b1b1b7701c32dac1baf7f3c45c328928ea0b
elinakocarslan <148897716+elinakocarslan@users.noreply.github.com>
2024-08-21 09:12:15 -0400

    fixed get involved page, but not completely

--- front_end/src/components/pages/GetInvolved.js
+++ front_end/src/components/pages/GetInvolved.js
@@ -18,34 +18,39 @@ function GetInvolved() {
                 </div>
             </div>
 
-            <div className='info-box'>
-                <div className='application-and-pic'>
+            <div className='wholeContainer'>
+
+                <div className='info-box'>
+                    <div className='application-and-pic'>
+                        <div className='caption'>
+                            <h1>APPLICATION</h1>
+                            <div className='paragraph'>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel nunc quis orci elementum rhoncus. Curabitur sodales velit felis, in vulputate mi porttitor ac. Maecenas quis nisl quis odio efficitur imperdiet. Proin ac gravida mi. Fusce interdum quam vitae odio tempor facilisis. Aliquam erat volutpat. Phasellus tristique turpis ut euismod consectetur. Donec et neque molestie eros dictum mattis sed quis risus. Curabitur faucibus malesuada urna, et gravida lectus fringilla non. Nunc pellentesque placerat faucibus. Fusce gravida odio non sapien aliquet vehicula.</p>
+                            </div>
+                        </div>
+                        <img src={randomImg} alt="Home Image" className='applicationPicImg' />
+                    </div>
+                </div>
+
+                <div className='FAQ-box'>
                     <div className='caption'>
-                        <h1>APPLICATION</h1>
-                        <div className='paragraph'>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel nunc quis orci elementum rhoncus. Curabitur sodales velit felis, in vulputate mi porttitor ac. Maecenas quis nisl quis odio efficitur imperdiet. Proin ac gravida mi. Fusce interdum quam vitae odio tempor facilisis. Aliquam erat volutpat. Phasellus tristique turpis ut euismod consectetur. Donec et neque molestie eros dictum mattis sed quis risus. Curabitur faucibus malesuada urna, et gravida lectus fringilla non. Nunc pellentesque placerat faucibus. Fusce gravida odio non sapien aliquet vehicula.</p>
-                        </div>
+                        <h1>FAQ</h1>
                     </div>
-                    <img src={randomImg} alt="Home Image" className='applicationPicImg' />
+
+                    {[...Array(5)].map((_, index) => (
+                        <div className='faq' key={index}>
+                            <button className='Q-Box' onClick={() => toggleFAQ(index)}>
+                                Q. Lorem ipsum dolor sit amet, consectetur adipiscing elit?
+                            </button>
+                            <div className={`answer ${openIndex === index ? 'active' : ''}`}>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
+
             </div>
 
-            <div className='FAQ-box'>
-                <div className='caption'>
-                    <h1>FAQ</h1>
-                </div>
-
-                {[...Array(5)].map((_, index) => (
-                    <div className='faq' key={index}>
-                        <button className='Q-Box' onClick={() => toggleFAQ(index)}>
-                            Q. Lorem ipsum dolor sit amet, consectetur adipiscing elit?
-                        </button>
-                        <div className={`answer ${openIndex === index ? 'active' : ''}`}>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
         </div>
     );
 }
