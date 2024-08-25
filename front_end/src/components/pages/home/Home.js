98c707cde76bdedb89266284bed4a73c6619f660
jonathandeng7 <jmdeng05@gmail.com>
2024-08-25 12:22:27 -0400

    home page update

--- front_end/src/components/pages/home/Home.js
+++ front_end/src/components/pages/home/Home.js
@@ -8,11 +8,10 @@ import peruTeams from '../../pictures/home/peru_teams.jpg';
 import ImagesComponent from './SponsorsHomePage';
 import videoEWB from "../../pictures/home/ewb_homepage.mp4";
 
-
 function Home() {
   return (
     <div className="home-container">
-      <div className = "home-img-container">
+      <div className="home-img-container">
         {/* <img src={homeImage} alt="UF EWB Logo" className="homeMainImage" /> */}
         <div className="video-container">
           <video width="100%" height="auto" autoPlay loop muted>
@@ -20,18 +19,16 @@ function Home() {
             Your browser does not support the video tag.
           </video>
         </div>
-        <h1 className = "textBeg"> ENGINEERING <br></br> WITHOUT BORDERS </h1>
-        <h1 className = "textBeg textSubtitle"> The University of Florida Chapter </h1>
+        <h1 className="textBeg"> ENGINEERING <br /> WITHOUT BORDERS </h1>
+        <h1 className="textBeg textSubtitle"> The University of Florida Chapter </h1>
       </div>
       
       <div className="teams">
-        <h1 className = "teamsHeading">OUR TEAMS</h1>
-        <p>🇵🇪 🇳🇵</p>
-
+        <h1 className="teamsHeading">OUR TEAMS</h1>
 
         <div className="team_indv">
           <img src={nepalTeams} alt="Nepal Team" className="team_img" />
-          <p className = "team_click underline-hover"> nepal </p>
+          <p className="team_click underline-hover">nepal</p>
         </div>
 
         <div className="team_indv">
@@ -40,9 +37,9 @@ function Home() {
         </div>
       </div>
 
-      <div className = "sponsors-container"> 
-        <h1 className = "teamsHeading">SPONSORS</h1>
-        {/* <button className = "sponsor-button"> Become a Sponsor</button>  */}
+      <h1 className="teamsHeading sponsors-title">SPONSORS</h1>  {/* Sponsors text moved outside of the box */}
+
+      <div className="sponsors-container"> 
         <ImagesComponent />
       </div>
 
