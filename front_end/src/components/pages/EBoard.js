69d935b6b660fbe53235a2e27a8d8ed4b572c610
elinakocarslan <148897716+elinakocarslan@users.noreply.github.com>
2024-08-27 16:12:11 -0400

    linked e-board page

---
+++ front_end/src/components/pages/EBoard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './EBoard.css';
+import { Link } from 'react-router-dom';
+
+function EBoard() {
+    return(
+        <div>
+        eboard yay
+
+        </div>
+    );
+}
+
+export default EBoard;
