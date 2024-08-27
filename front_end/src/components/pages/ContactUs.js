57583be09fc20ef3b91890330288e41bee9be900
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2024-08-27 11:52:46 -0400

    updates to css grid on the eboard page

--- front_end/src/components/pages/ContactUs.js
+++ front_end/src/components/pages/ContactUs.js
@@ -6,17 +6,15 @@ import linkedinLogo from "../pictures/footer/linkedin-logo.png";
 
 function ContactUs() {
     return (
-        <div>
-            <div className="container">
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
-                <div>4</div>
-                <div>5</div>
-                <div>6</div>
-                <div>7</div>
-                <div>8</div>
-            </div>
+        <div class="container">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+            <div>7</div>
+            <div>8</div>
         </div>
     );
 }
