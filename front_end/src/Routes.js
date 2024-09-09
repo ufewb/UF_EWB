9c09cfe89c797726d6be721c0f774a86c60b49ff
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2024-09-09 03:43:50 -0400

    finished responsiveness for get involved

--- front_end/src/Routes.js
+++ front_end/src/Routes.js
@@ -4,7 +4,7 @@ import Home from './components/pages/home/Home';
 import GetInvolved from './components/pages/get-involved/GetInvolved';
 import NepalHome from './components/pages/nepal/NepalHome';
 import PeruHome from './components/pages/peru/PeruHome';
-import FAQ from './components/pages/faq/FAQ';
+// import FAQ from './components/pages/faq/FAQ';
 import Login from './components/pages/login/Login';
 import PeruRoutes from './components/pages/peru/PeruRoutes';
 import NepalRoutes from './components/pages/nepal/NepalRoutes';
@@ -17,7 +17,7 @@ function Paths() {
       <Route path="/get-involved" element={<GetInvolved />} />
       <Route path="/nepal/*" element={<NepalRoutes />} />
       <Route path="/peru/*" element={<PeruRoutes />} />
-      <Route path="/faq" element={<FAQ />} />
+      {/* <Route path="/faq" element={<FAQ />} /> */}
       <Route path="/login" element={<Login />} />
       <Route path="/eboard" element={<EBoard />} />
     </Routes>
